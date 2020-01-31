a2ef701ab88a23c0f3cabcbbd43a29a7137c0c25
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-01-31 10:37:14 -0500

    Complete maxValue problem using depth-first search

--- graph_project/lib/max_value.js
+++ graph_project/lib/max_value.js
@@ -1,6 +1,36 @@
+// class GraphNode {
+//     constructor(val) {
+//         this.val = val;
+//         this.neighbors = [];
+//     }
+// }
+
 function maxValue(node, visited=new Set()) {
+    let largestVal = node.val;
+
+    function depthFirstSearch(node, visited) {
+        if (node.val > largestVal) {
+            largestVal = node.val;
+        }
+
+        if (!visited.has(node.val)) {
+            visited.add(node.val);
+            node.neighbors.forEach(neighbor => {
+                depthFirstSearch(neighbor, visited);
+            })
+        }
+    }
+
+    visited.add(node.val);
+
+    node.neighbors.forEach(neighbor => {
+        depthFirstSearch(neighbor, visited);
+    })
+
+    return largestVal;
 
 }
+
 
 module.exports = {
     maxValue
