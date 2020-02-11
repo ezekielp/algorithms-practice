86c12d16b5e388e250210ecb35cc2b3da2a86733
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-02-11 16:13:26 -0500

    Keep working on first_and_last_in_sorted_array LC problem

--- first_and_last_in_sorted_array.js
+++ first_and_last_in_sorted_array.js
@@ -11,7 +11,28 @@ let nums = [5, 7, 7, 8, 8, 10];
 let target = 8;
 
 const searchRange = (nums, target) => {
-    
+    let startNumRange = [0, nums.length - 1];
+    let startNumFound = false;
+    let startIdx = Math.floor(nums.length / 2);
+
+    while (!startNumFound && startNumRange.length > 0) {
+        let currentStartIdx = startNumRange[0];
+        let currentEndIdx = startNumRange[1];
+        let currentRange = nums.slice(currentStartIdx, currentEndIdx);
+
+        let numToCheck = currentRange[startIdx];
+        if (numToCheck === target) {
+            startNumFound = true;
+        } else if (numToCheck < target) {
+            startNumRange[1] = Math.floor()
+        } else {
+            startNumRange[0] = Math.floor()
+        }
+    }
+
+
+ 
+
 };
 
 
