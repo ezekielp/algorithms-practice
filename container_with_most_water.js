7c289de60384eb9199bf789860491c4c63fc18b3
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-02-11 14:31:35 -0500

    Refactor container_with_most_water LC problem solution

--- container_with_most_water.js
+++ container_with_most_water.js
@@ -6,31 +6,60 @@
 @return {number}
 */
 
-let testHeights = [1, 8, 6, 2, 5, 4, 8, 3, 7];
-
+let testHeights = [2, 3, 10, 5, 7, 8, 9];
 
 const maxArea = heights => {
-    let startIdx = 0;
     let endIdx = heights.length - 1;
-    let incrementStartIdxNext = true;
-    let largestAreaSoFar = 0;
+    let largestArea = 0;
 
-    while (startIdx < endIdx) {
-        let startingHeight = heights[startIdx];
-        let endingHeight = heights[endIdx];
+    while (endIdx > 0) {
 
-        let height = Math.min(startingHeight, endingHeight);
-        let width = endingHeight - startingHeight;
+        for (let startIdx = 0; startIdx < endIdx; startIdx++) {
+            let startingHeight = heights[startIdx];
+            let endingHeight = heights[endIdx];
 
-        if ((height * width) > largestAreaSoFar) {
-            largestAreaSoFar = height * width;
-        } 
+            let height = Math.min(startingHeight, endingHeight);
+            let width = endIdx - startIdx;
 
-        if (incrementStartIdxNext) {
-            incrementStartIdxNext = false;
-        } else {
-            incrementStartIdxNext = true;
+            if ((height * width) > largestArea) {
+                largestArea = height * width;
+            } 
         }
-    }
+
+        endIdx = endIdx - 1;
+    }    
+
+    return largestArea;
 };
+
+console.log(maxArea(testHeights));
+
+// const maxArea = heights => {
+//     let startIdx = 0;
+//     let endIdx = heights.length - 1;
+//     let incrementStartIdxNext = true;
+//     let largestArea = 0;
+
+//     while (startIdx < endIdx) {
+//         let startingHeight = heights[startIdx];
+//         let endingHeight = heights[endIdx];
+
+//         let height = Math.min(startingHeight, endingHeight);
+//         let width = endIdx - startIdx;
+
+//         if ((height * width) > largestArea) {
+//             largestArea = height * width;
+//         } 
+
+//         if (incrementStartIdxNext) {
+//             startIdx = startIdx + 1;
+//             incrementStartIdxNext = false;
+//         } else {
+//             endIdx = endIdx - 1;
+//             incrementStartIdxNext = true;
+//         }
+//     }
+
+//     return largestArea;
+// };
 
