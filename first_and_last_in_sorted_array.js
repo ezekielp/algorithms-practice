2477cd9affef8012f41a44684b725b225b203879
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-02-11 15:15:28 -0500

    Create file for first_and_last_in_sorted_array LC problem

---
+++ first_and_last_in_sorted_array.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+
+/*
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+let nums = [5, 7, 7, 8, 8, 10];
+let target = 8;
+
+const searchRange = (nums, target) => {
+    
+};
+
+
+
