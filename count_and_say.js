a851fe1956c330215a672a048876f61fe7508f26
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-02-17 15:58:24 -0500

    Try long-winded solution to count_and_say, then quickly rethink and start refactoring

--- count_and_say.js
+++ count_and_say.js
@@ -3,9 +3,43 @@
  * @return {string}
  */
 
-const countAndSay = (n) => {
+/*
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+*/
 
-    
+
+const countAndSay = (n) => {
+    if (n === 1) return "1";
+
+    const previous = countAndSay(n - 1);
+    const partsOfPrevious = [];
+    const stack = [];
+    let result = "";
+
+    for (let i = 0; i < previous.length; i++) {
+        
+    }
+
+
+    // let currentChar, currentChunk;
+    // let i = 0;
+    // currentChunk = "";
+
+    // while (i < previous.length) {
+    //     if (currentChunk.length === 0) {
+    //         currentChunk = previous[i];
+    //         i++;
+    //     }
+    //     while (currentChunk[currentChunk.length - 1] === previous[i]) {
+
+    //     }
+    // }
+        
+
 
 
 };
