b8b781fb80d0720bb091a21accfe1729c1205d8e
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-02-19 12:13:14 -0500

    Get mostly working solution to reverse_linked_list

--- reverse_linked_list.js
+++ reverse_linked_list.js
@@ -14,17 +14,20 @@
 // Output: 5 -> 4 -> 3 -> 2 -> 1 -> NULL
 
  var reverseList = function (head) {
-    let previous = head; // 1
-    previous.next = null; // 1 -> NULL
-    let current = head.next; // 2 (-> 3)
-    let next = current.next; // 3 (-> 4)
-    current.next = previous; // 2 -> 1 -> NULL
+    if (head.next === null) return head;
+
+
+    let previous = head; 
+    let current = head.next; 
+    let next = current.next; 
+    previous.next = null; 
+    current.next = previous; 
 
     while (next.next !== null) {
-        previous = current; // 2 // 3
-        current = next; // 3 (-> 4) // 4 (-> 5)
-        next = current.next; // 5 (-> NULL)
-        current.next = previous; // 3 -> 2 -> 1 -> NULL // 4 -> 3 -> 2 -> 1 -> NULL
+        previous = current; 
+        current = next; 
+        next = current.next; 
+        current.next = previous;
     }
 
     next.next = current;
@@ -32,6 +35,21 @@
 
 };
 
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
 
+let node1 = new ListNode(1);
+let node2 = new ListNode(2);
+let node3 = new ListNode(3);
+let node4 = new ListNode(4);
+let node5 = new ListNode(5);
 
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
 
+console.log(node1);
+console.log(reverseList(node1));
