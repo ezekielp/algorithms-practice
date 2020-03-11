d3a212ea9382ee9d988cfc0756ce2e29b2ef5dce
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-03-10 21:48:58 -0400

    Work on BST project

--- bst_project/lib/bst.js
+++ bst_project/lib/bst.js
@@ -8,8 +8,52 @@ class TreeNode {
 
 
 class BST {
-   
+   constructor() {
+       this.root = null;
+   }
+
+   insert(val) {
+       if (this.root === null) { // If no root yet, make this val the root
+           this.root = new TreeNode(val);
+       } else { // If there is a root, look for where to insert this val
+            let previousNode = this.root;
+            let currentNode;
+            if (val < this.root.val) { // If val is less than root val, go left
+                currentNode = previousNode.left;
+            } else { // Otherwise go right
+                currentNode = previousNode.right;
+            }
+            while (currentNode !== null) {
+                if (val < currentNode.val) {
+                    previousNode = currentNode;
+                    currentNode = currentNode.left;
+                } else {
+                    previousNode = currentNode;
+                    currentNode = currentNode.right;
+                }
+            }
+            if (val < previousNode.val) {
+                previousNode.left = new TreeNode(val);
+            } else {
+                previousNode.right = new TreeNode(val);
+            }
+       }
+   }
+
+   searchRecur(val) {
+
+   }
+
+   searchIter(val) {
+
+   }
+
 }
+
+let newBST = new BST();
+newBST.insert(8);
+newBST.insert(3);
+newBST.insert(6);
 
 module.exports = {
     TreeNode,
